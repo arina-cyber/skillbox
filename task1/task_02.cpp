a74376d5d1283910f948c5770d1a9a73e622b3ce
arina-cyber <arisha-live@yandex.ru>
2022-02-07 12:03:37 +0300

    task1

--- task1/task_02.cpp
+++ task1/task_02.cpp
@@ -16,9 +16,9 @@ bool allowmin (char email) {
     for (int j = 0; j < allow.length(); j++) {
         if (email == allow [j]) {
             symbol = true;
-            break;
         } else {
             symbol = false;
+            break;
         }
     }
     return symbol;
@@ -30,9 +30,9 @@ bool allow (char email) {
     for (int j = 0; j < allow.length(); j++) {
         if (email == allow [j]) {
             symbol = true;
-            break;
         } else {
             symbol = false;
+            break;
         }
     }
     return symbol;
@@ -40,7 +40,7 @@ bool allow (char email) {
 
 bool first_part_check(string email) {
     bool result = true;
-    for (int i = 0; email[i] != '@'&& i<63; i++) {
+    for (int i = 0; i < email.length(); i++) {
         if (email[0] == '.') {
             result = false;
         }
@@ -56,7 +56,7 @@ bool first_part_check(string email) {
 
 bool second_part_check(string email) {
     bool result = true;
-    for (int i = 0; email[i] != '@'&& i<63; i++) {
+    for (int i = 0; i < email.length(); i++) {
         if (email[0] == '.') {
             result = false;
         }
@@ -76,17 +76,28 @@ void task2() {
     string first_part;
     string second_part;
     int lasti;
+    bool check = false;
     cout << "Введите email: ";
     getline(cin, email);
-    for (int i = 0; email[i] != '@'&& i<63; i++) {
-        first_part += email[i];
-        lasti=i;
+    for (int i = 0; i < email.length(); i++) {
+        if (email[i] == '@') check = true; break;
     }
-    for (int i = lasti+2; email.length(); i++) {
-        second_part += email[i];
+    if (check == 0) {
+        cout << "NO";
+    } else if (email.length()>63) {
+        cout << "NO";
+    } else {
+        for (int i = 0; email[i] != '@' && i < 63; i++) {
+            first_part += email[i];
+            lasti = i;
+        }
+       // cout << first_part << endl;
+        for (int i = lasti + 2; i < email.length(); i++) {
+            second_part += email[i];
+        }
+       // cout << second_part << endl;
+        if (first_part_check(first_part) && second_part_check(second_part)) {
+            cout << "YES" << endl;
+        } else cout << "NO" << endl;
     }
-    if (first_part_check(first_part) && second_part_check(second_part)) {
-        cout << "YES" << endl;
-    } else cout << "NO" << endl;
-
 }
